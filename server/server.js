eee76cfa1ec6ffbbcacf34da77c782a1f5fd16d7
Jane Kagan <kagan009@umn.edu>
2021-11-19 06:20:20 -0600

    Back to express

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const path = require('path');
+const app = express();
 
-const port = process.env.PORT || 3000;
+app.use(express.static(path.join(__dirname, 'build')));
 
-var app = express();
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+});
 
-app.listen(port);
+app.listen(9000);
